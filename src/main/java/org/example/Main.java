769ffb0428ee44bd01c10930de03cb9f3bed2bd6
Vyacheslav Markov <mvr.here@vk.com>
2025-10-21 11:04:38 +0600

    init dev

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,7 +3,7 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.printf("Hello and welcome!");
-        System.out.println("test ssh");
+        //init dev
 
         for (int i = 1; i <= 5; i++) {
             System.out.println("i = " + i);
