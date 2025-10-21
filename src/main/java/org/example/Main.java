8268fd72b5b5d2ec8148ba15f36832b77e846c39
miron <mironmoskalenko2000@gmail.com>
2025-10-21 11:20:09 +0600

    A lot of umls

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,7 +2,7 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("Hello and welcome!");
+        System.out.print("Hello and welcome!");
         //init dev
 
         for (int i = 1; i <= 5; i++) {
